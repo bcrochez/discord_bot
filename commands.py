672d6f9f33ab8565ad28f0a4c8e5acedab2a94bb
Bastien Crochez <bastien.crochez@quickign.com>
2019-11-05 11:54:45 +0100

    add command

--- commands.py
+++ commands.py
@@ -20,6 +20,11 @@ def get_commands(bot, logger):
         await ctx.send("(╯°□°）╯︵ ┻━┻")
 
     @bot.command()
+    async def vache(ctx):
+        """Flips a table"""
+        await ctx.send("(╯°□°）╯︵ ┻━┻")
+
+    @bot.command()
     async def add(ctx, left: int, right: int):
         """Adds two numbers together."""
         await ctx.send(left + right)
